754b22640e69b6e773d9f1228d44a5b613e61abd
浤宬 蕭 <108111118@mail.aeust.edu.tw>
2021-10-22 09:27:41 +0800

    123

---
+++ 110-1HW2/StuScore.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1HW2
+{
+    public partial class StuScore : System.Web.UI.Page
+    {
+        public string s_ID;
+        public float f_ChiS;
+        public float f_MathS;
+        public float f_EngS;
+    }
+    protected void Page_Load(object sender, EventArgs e)
+        {
+            Scores o_scores = new Scores();
+            o_scores.s_ID = "A123456789";
+            o_scores.f_ChiS = 62f;
+            o_scores.f_MathS = 73f;
+            o_scores.f_EngS = 84f;
+
+            Response.Write("s_ID=" + o_scores.s_ID + "," +
+                "f_ChiS=" + o_scores.f_ChiS.ToString() + "," +
+                "f_MathS=" + o_scores.f_MathS.ToString() + "," +
+                "f_EngS=" + o_scores.f_EngS.ToString());
+        }
+    }
+}
